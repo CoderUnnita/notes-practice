7106245587a21a7499d47500c19ea0d2ee3012cb
UnnitaYaduvanshi <unnitayaduvanshi@gmail.com>
2024-12-17 14:41:21 +0530

    added files

--- javascript.js
+++ javascript.js
@@ -2,17 +2,19 @@
 
 const name = "Unnita"
 let roll = 1234
-var dish = 'Jalebi Gol Gappe  '
+var dish = 'Jalebi Gol Gappe'
 
 state = " Delhi "
 
-// name = 23 // not applicable 
+// name = 23 // not applicable as it is a const
 
+roll = 539576
+dish = 'dairy milk'
 
+state = 'Berlin'
 
-
-
-
+console.table([roll, dish])
+console.log(state)
 
 
 console.log("Hello Let's Learn JavaScript");
@@ -20,14 +22,17 @@ const age=19;
 console.log("Age is", age);
 
 //Array using let
+
 let arr=[12,14,16,18,20];
 console.log(arr[1]); 
 
 //Array using const
+
 const arr1=[12,14,16,18,20];
 console.log(arr[0]); 
 
 // Conditionals Statements
+
 const $age=14;
 if ($age<18){
     console.log("Not Eligible!");
