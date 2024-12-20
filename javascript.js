82f065196d6cc4daa474e462311cf2aa17febb80
UnnitaYaduvanshi <unnitayaduvanshi@gmail.com>
2024-12-20 18:42:15 +0530

    simpliying view

--- javascript.js
+++ javascript.js
@@ -42,20 +42,18 @@ console.log("Hello Let's Learn JavaScript");
 const age=19;
 console.log("Age is", age);
 
-//Array using let
 
+//Array using let
 let arr=[12,14,16,18,20];
 console.log(arr[1]); 
 
 
 //Array using const
-
 const arr1=[12,14,16,18,20];
 console.log(arr[0]); 
 
 
 // Conditionals Statements
-
 const $age=14;
 if ($age<18){
     console.log("Not Eligible!");
