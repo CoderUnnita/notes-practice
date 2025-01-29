7ea50ddefdb67b26a3e1c86c4ad1599572beba32
UnnitaYaduvanshi <unnitayaduvanshi@gmail.com>
2025-01-29 19:03:17 +0530

    inspi-link

---
+++ prac.py
@@ -0,0 +1,31 @@
+fruits = [1,2, 3, 4, 6]
+
+for x in fruits:
+  if x%3==0:
+    fruits.remove(x)
+
+print(fruits)
+
+
+def check(x,y,z):
+    list = [x,y,z]
+
+    if list[0] < list [1]:
+        if list[1]< list[2]:
+            print("TRUE")
+        else:
+            print("FALSE")
+    else:
+        print("FALSE")
+
+
+
+numbers= check(13,14,15)
+
+fruits = [1,2, 3, 4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+for x in fruits:
+  if x%3==0:
+    fruits.remove(x)
+
+print(fruits)
